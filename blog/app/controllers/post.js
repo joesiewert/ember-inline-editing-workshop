20187404c2e4b73b259bac9dbbe7b49d3250001a
joesiewert <joe.siewert@gmail.com>
2015-02-27 22:38:31 -0700

    Setup inline editing

--- blog/app/controllers/post.js
+++ blog/app/controllers/post.js
@@ -1,7 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-  actions: {
+  isEditing: false,
 
+  actions: {
+    editPost: function() {
+      this.set('isEditing', true);
+    },
+    savePost: function(post) {
+      var body  = post.get('body'),
+          title = post.get('title');
+
+      if (body && title) {
+        this.set('isEditing', false);
+        post.save();
+      }
+    }
   }
 });
